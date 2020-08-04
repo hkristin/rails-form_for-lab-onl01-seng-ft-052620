048a0aea0426c694da68d44cbeab8b2e8167f3cf
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-04 02:26:45 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -7,7 +7,7 @@ class SchoolClassesController < ApplicationController
   def create
     @school_class = SchoolClass.new(school_class_params)
     @school_class.save
-    redirect_to school_class_paths(@school_class)
+    redirect_to school_class_path(@school_class)
   end
 
   def show
@@ -21,7 +21,7 @@ class SchoolClassesController < ApplicationController
   def update
     @school_class = SchoolClass.find(params[:id])
     @school_class.update(school_class_params)
-    redirect_to school_class_paths(@school_class)
+    redirect_to school_class_path(@school_class)
   end
 
   private

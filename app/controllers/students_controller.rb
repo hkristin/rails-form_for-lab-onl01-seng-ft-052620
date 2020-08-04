c5ec4f6e233f19bd0f14e54634cce96bf9a86982
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-04 02:10:14 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,10 +14,18 @@ class StudentsController < ApplicationController
   end
 
   def edit
-
+    @student = Student.find(params[:id])
   end
 
   def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+    redirect_to student_path(@student)
+  end
+  
+  private
 
+  def student_params
+   params.require(:student).permit!
   end
 end
